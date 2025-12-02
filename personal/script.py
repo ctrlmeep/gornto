92b2682e602b59ddfaca671be7e315ba17c93132
ctrlmeep <benlandon1017@gmail.com>
2025-12-02 10:35:06 -0500

    n/a

--- personal/script.py
+++ personal/script.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 def number_clicked():
+
     """
     number_clicked
     """
@@ -9,14 +10,35 @@ root = tk.Tk()
 root.title("Calculator")
 root.geometry("300x400")
 
-column = 1
-row = 1
+
+display = ""
+text = tk.Label(
+    root,
+    text=display,
+    bg="white",
+    fg="black",
+    font=("Arial", 12),
+    width=20,
+    height=20
+
+)
+
+column = 0
+startingRow = 2
+row = startingRow
 for i in range(10):
     button = tk.Button(
         root,
-        text=str(i),
-        command = number_clicked,
-    )
+        name="0" if i == 9 else str(i + 1),
+        text= "0" if i == 9 else str(i + 1),
+        command = number_clicked
 
-    button.grid(row=i, column=0)
+    )
+    column += 1
+    if column == 4:
+        row += 1
+        if row == 4: column = 2
+        else: column = 1
+    button.grid(row=row, column=column)
+
 root.mainloop()

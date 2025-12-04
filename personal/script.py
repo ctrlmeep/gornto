ee88594e2c3214623277926e8dfc6f3f445fb2f5
ctrlmeep <benlandon1017@gmail.com>
2025-12-04 13:58:35 -0500

    super mega duper calculator
    Signed-off-by: ctrlmeep <benlandon1017@gmail.com>

--- personal/script.py
+++ personal/script.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 operations = ["+", "-", "x", "÷"]
 
 def pos_or_neg(num):
@@ -25,7 +25,6 @@ class Calculator:
 
     def setup_window(self):
         self.root.title("Calculator")
-        self.root.geometry("300x400")
 
     def calculate(self):
         if self.num1.endswith("."):
@@ -35,11 +34,11 @@ class Calculator:
 
         if "%" in self.num1:
             print("% found in num1, attempting to format")
-            self.num1.replace("%", "")
+            self.num1 = self.num1.replace("%", "")
             self.num1 = float(self.num1) / 100
         if "%" in self.num2:
             print("% found in num2, attempting to format")
-            self.num2.replace("%", "")
+            self.num2 = self.num2.replace("%", "")
             self.num2 = float(self.num2) / 100
 
         if self.num1 == "-":
@@ -50,39 +49,42 @@ class Calculator:
         float1 = float(self.num1)
         float2 = float(self.num2)
         result = ""
+        div_zero_error = False
         if self.operation == "+": result = float1 + float2
         elif self.operation == "-": result = float1 - float2
         elif self.operation == "x": result = float1 * float2
-        elif self.operation == "÷": result = float1 / float2
+        elif self.operation == "÷":
+            if float2 != 0: result = float1 / float2
+            else:
+                print("Can't divide by zero, nothing happened.")
+                div_zero_error = True
         else: print("Invalid operation")
-        if result.is_integer(): result = int(result)
-        result = str(result)
-        self.num1 = result
-        self.operation = ""
-        self.num2 = ""
-        self.stage = 1
+        if not div_zero_error:
+            if result.is_integer(): result = int(result)
+            result = str(result)
+            self.num1 = result
+            self.operation = ""
+            self.num2 = ""
+            self.stage = 1
 
     def staging_error(self):
         print(f"Something went wrong with staging, current in stage: {self.stage}")
 
     def button_clicked(self, value):
         if value in numbers:
-            if self.stage == 1:
-                self.num1 += str(value)
-
-            elif self.stage == 2:
-                self.num2 += str(value)
+            if self.stage == 1: self.num1 += str(value)
+            elif self.stage == 2: self.num2 += str(value)
             else: self.staging_error()
 
         elif value in operations:
             if self.stage == 1:
-                self.operation = value
-                self.stage = 2
-            elif self.stage == 2:
-                print("Input operation, nothing happened")
+                if self.num1 != "":
+                    self.operation = value
+                    self.stage = 2
+            elif self.stage == 2: print("Input operation, nothing happened")
             else: self.staging_error()
 
-        elif value == "DELETE":
+        elif value == "<x":
             if self.stage == 1:
                 if len(self.num1) < 1:
                     print("Nothing in entry, nothing was done.")
@@ -104,13 +106,17 @@ class Calculator:
             self.stage = 1
 
         elif value == "%":
-            if self.stage == 1: self.num1 += "%"
-            elif self.stage == 2: self.num2 += "%"
+            if self.stage == 1:
+                if "%" not in self.num1: self.num1 += "%"
+            elif self.stage == 2:
+                if "%" not in self.num2: self.num2 += "%"
             else: self.staging_error()
 
         elif value == ".":
-            if self.stage == 1: self.num1 += "."
-            elif self.stage == 2: self.num2 += "."
+            if self.stage == 1:
+                if "." not in self.num1: self.num1 += "."
+            elif self.stage == 2:
+                if "." not in self.num2: self.num2 += "."
             else: self.staging_error()
 
         elif value == "+/-":
@@ -126,7 +132,8 @@ class Calculator:
                     self.num2 = self.num2[1:]
             else: self.staging_error()
 
-        elif value == "=": self.calculate()
+        elif value == "=":
+            if self.num1 != "" and self.operation != "" and self.num2 != "": self.calculate()
         else: print("Input error")
 
         self.display_label.config(text=self.num1 + self.operation + self.num2)
@@ -139,114 +146,27 @@ class Calculator:
         print(f"Stage: {self.stage}")
 
     def create_widgets(self):
-        #display label
-        self.display_label = tk.Label(
-            self.root,
-            text="",
-            bg="gray",
-            fg="black",
-            font=("Arial", 12),
-            width=20,
-            height=2,
-        )
-        self.display_label.grid(column=2, row=0, columnspan=2)
+        self.display_label = tk.Label(self.root, bg="white", fg="black", height=2, width=24)
+        self.display_label.grid(column=1, row=1, columnspan=4)
 
-        #number buttons
-        starting_column = 1
-        column = starting_column + 3
-        starting_row = 3
-        row = starting_row
-        for i in range(10):
+        buttons = ["<x", "AC", "%", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="]
+
+        column = 0
+        row = 2
+        for i in buttons:
             button = tk.Button(
                 self.root,
-                text=str(9 - i),
-                command=lambda num=9 - i: self.button_clicked(num),
+                text = i,
+                command = lambda n = i: self.button_clicked(n),
+                height = 2,
+                width = 2,
             )
-            column -= 1
-            if column == 0:
+            column += 1
+            if column == 5:
                 row += 1
-                if row == starting_row + 3:
-                    column = 2
-                else:
-                    column = 3
-            button.grid(row=row, column=column)
+                column = 1
+            button.grid(column=column, row=row)
 
-        #operator buttons
-        addition = tk.Button(
-            self.root,
-            text="+",
-            command=lambda: self.button_clicked("+"),
-        )
-        addition.grid(row=starting_row + 2, column=starting_column + 3)
+    def run(self): self.root.mainloop()
 
-        subtraction = tk.Button(
-            self.root,
-            text="-",
-            command=lambda: self.button_clicked("-"),
-        )
-        subtraction.grid(row=starting_row + 1, column=starting_column + 3)
-
-        multiplication = tk.Button(
-            self.root,
-            text="x",
-            command=lambda: self.button_clicked("x"),
-        )
-        multiplication.grid(row=starting_row, column=starting_column + 3)
-
-        division = tk.Button(
-            self.root,
-            text="÷",
-            command=lambda: self.button_clicked("÷"),
-        )
-        division.grid(row=starting_row - 1, column=starting_column + 3)
-
-        equal = tk.Button(
-            self.root,
-            text="=",
-            command=lambda: self.button_clicked("="),
-        )
-        equal.grid(row=starting_row + 3, column=starting_column + 3)
-
-        percent = tk.Button(
-            self.root,
-            text="%",
-            command=lambda: self.button_clicked("%"),
-        )
-        percent.grid(row=starting_row - 1, column=starting_column + 2)
-
-        ac = tk.Button(
-            self.root,
-            text="AC",
-            command=lambda: self.button_clicked("AC"),
-        )
-        ac.grid(row=starting_row - 1, column=starting_column + 1)
-
-        delete = tk.Button(
-            self.root,
-            text="<x",
-            command=lambda: self.button_clicked("DELETE"),
-        )
-        delete.grid(row=starting_row - 1, column=starting_column)
-
-        decimal = tk.Button(
-            self.root,
-            text=".",
-            command=lambda: self.button_clicked("."),
-        )
-        decimal.grid(row=starting_row + 3, column=starting_column + 2)
-
-        posNeg = tk.Button(
-            self.root,
-            text="+/-",
-            command=lambda: self.button_clicked("+/-"),
-        )
-        posNeg.grid(row=starting_row + 3, column=starting_column)
-
-
-    def run(self):
-        self.root.mainloop()
-
-
-if __name__ == '__main__':
-    app = Calculator()
-    app.run()
+if __name__ == '__main__': Calculator().run()
